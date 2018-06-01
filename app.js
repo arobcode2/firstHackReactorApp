a28739eeb9fb79acaec33b17f38f38ea5777d30b
Andrew Rob <andrew-c-rob@live.com>
2018-06-01 12:12:40 -0500

    Added a feature that saves data to local storage

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
 
-	$('.textField').on('click', function() {
+	$('.submitForm').on('click', function() {
+		let textFieldValue = $('.textField').val();
+		$('.debug').text(textFieldValue);
+
+		localStorage.setItem('myFormTextData', textFieldValue);
+
+	});
+
+	$('.textField').on('keyup', function() {
 		let textFieldValue = $('.textField').val();
 		$('.debug').text(textFieldValue);
 	});
+
 
 });
